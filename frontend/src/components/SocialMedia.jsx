b6e226346eb028afe427705acd428520b51b39bc
Nicholas Markus <nicxzmiller2@gmail.com>
2022-03-07 15:26:32 +0100

    adding testimonials and fixing social media links

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsTwitter, BsBehance, BsGithub, BsDribbble} from 'react-icons/bs'
+import { BsTwitter, BsBehance, BsGithub, BsDribbble, BsLinkedin} from 'react-icons/bs'
 
 const SocialMedia = () => {
   return (
@@ -8,14 +8,37 @@ const SocialMedia = () => {
             <BsTwitter/>
         </div>
         <div>
-            <BsBehance/>
+            <>
+                <a href="https://behance.net/Nicxzmiller" target="_blank" rel="noreferrer">
+                    <BsBehance/>
+                </a>
+            </>
         </div>
+
         <div>
-            <BsGithub/>
+            <>
+                <a href="https://github.com/Nicxzmiller" target="_blank" rel="noreferrer">
+                    <BsGithub/>
+                </a>    
+            </>
         </div>
+
         <div>
-            <BsDribbble/>
+            <>
+                <a href="https://dribbble.com/nicxzmiller" target="_blank" rel="noreferrer">
+                <BsDribbble/>
+                </a>    
+            </>
         </div>
+
+        <div>
+            <>
+                <a href="https://www.linkedin.com/in/nicholas-chun-markus-0b0136a2/" target="_blank" rel="noreferrer">
+                    <BsLinkedin/>
+                </a>
+            </>
+        </div>
+        
     </div>
   )
 }
